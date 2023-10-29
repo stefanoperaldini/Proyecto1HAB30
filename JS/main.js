90d5638991a2a909a016d4b6ef6a0e27d28145a1
Carlos <marcosmesgratis@hotmail.es>
2023-10-29 17:55:38 +0100

    JavaScript base

--- JS/main.js
+++ JS/main.js
@@ -1,3 +1,31 @@
-"use strict";
-console.log("hola")
 
+// Obtén el formulario de búsqueda y agrega un controlador de eventos para la búsqueda
+const searchForm = document.getElementById('flight-search-form');
+searchForm.addEventListener('submit', function (e) {
+    e.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+
+    // Obtén los valores de entrada del formulario
+    const origin = document.getElementById('origin').value;
+    const destination = document.getElementById('destination').value;
+    const departureDate = document.getElementById('departure-date').value;
+    const returnDate = document.getElementById('return-date').value;
+    const passengers = document.getElementById('passengers').value;
+
+    // Aquí puedes realizar una solicitud a tu servidor o una API de búsqueda de vuelos
+    // y mostrar los resultados en la sección de resultados de búsqueda.
+    // Puedes usar AJAX, Fetch API u otras tecnologías para esto.
+    // Por ejemplo:
+    fetch(`/search-flights?origin=${origin}&destination=${destination}&departureDate=${departureDate}&returnDate=${returnDate}&passengers=${passengers}`)
+        .then(response => response.json())
+        .then(data => {
+            const resultsContainer = document.getElementById('search-results');
+            resultsContainer.innerHTML = ''; // Limpia los resultados anteriores
+            data.forEach(result => {
+                // Crea y agrega elementos HTML para mostrar cada resultado de vuelo
+                // resultsContainer.appendChild(...);
+            });
+        })
+        .catch(error => {
+            console.error(error);
+        });
+});
