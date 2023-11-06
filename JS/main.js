85f51b229a9a7c0defd88c7303c6ec780fcea4d2
Luis Javier Castillo Afonso <javi.ca@MacBook-Pro-de-Luis.local>
2023-11-06 02:19:26 +0000

    mostrar resultado de búsqueda js y estilos

--- JS/main.js
+++ JS/main.js
@@ -2,7 +2,7 @@
 
 async function getAmadeusToken(clientId, clientSecret) {
   try {
-    // URL de autenticación de Amadeus (puede variar en producción)
+    
     const authUrl = "https://test.api.amadeus.com/v1/security/oauth2/token";
 
     const response = await fetch(authUrl, {
@@ -14,7 +14,7 @@ async function getAmadeusToken(clientId, clientSecret) {
     });
 
     if (!response.ok) {
-      // Visualizarlo en la página HTML
+      
       throw new Error("Error al obtener el token de Amadeus");
     }
 
@@ -22,7 +22,7 @@ async function getAmadeusToken(clientId, clientSecret) {
 
     return data.access_token;
   } catch (error) {
-    // Visualizarlo en la página HTML
+    
     console.error("Error:", error.message);
     return null;
   }
@@ -30,21 +30,22 @@ async function getAmadeusToken(clientId, clientSecret) {
 
 const searchForm = document.getElementById("flight-search-form");
 searchForm.addEventListener("submit", async function (e) {
-  e.preventDefault(); // Evita que el formulario se envíe de forma predeterminada
+  e.preventDefault();
+  document.getElementById("loading-message").style.display = "block";
 
-  // Obtén los valores de entrada del formulario
+  
   const origin = document.getElementById("origin").value;
   const destination = document.getElementById("destination").value;
 
   console.log(origin);
   console.log(destination);
 
-  const outputDiv = document.getElementById("output");
 
-  async function searchFlights(token, origin, destination, departureDate) {
-    // Define la URL de la solicitud de búsqueda de vuelo aquí
-    const url = `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${origin}&destinationLocationCode=${destination}&departureDate=${departureDate}&adults=${passengers}&nonStop=false&max=250`;
 
+  async function searchFlights(token, origin, destination, departureDate, passengers) {
+    
+    const url = `https://test.api.amadeus.com/v2/shopping/flight-offers?originLocationCode=${origin}&destinationLocationCode=${destination}&departureDate=${departureDate}&adults=${passengers}&nonStop=false&max=1`;
+  
     try {
       const response = await fetch(url, {
         headers: {
@@ -52,32 +53,53 @@ searchForm.addEventListener("submit", async function (e) {
         },
         mode: "cors",
       });
-
+  
       if (!response.ok) {
-        // Visualizarlo en la página HTML
-        throw new Error("Error al obtener el vuelo");
+        
+        document.getElementById("error-message").textContent = "Error al obtener el vuelo";
+        return;
       }
-
+  
       const datosVuelo = await response.json();
       console.log(datosVuelo);
-
+  
+      if (datosVuelo.data === 0) {
+        
+        document.getElementById("error-message").textContent = "No se han encontrado ningún vuelo";
+        return;
+      }
+  
       const vueloBarato = datosVuelo.data[0];
       console.log(vueloBarato);
+  
+      document.getElementById("flight-origin").textContent = origin;
+      document.getElementById("flight-destination").textContent = destination;
+      document.getElementById("flight-departure").textContent = vueloBarato.itineraries[0].segments[0].departure.at;
+      document.getElementById("flight-arrival").textContent = vueloBarato.itineraries[0].segments[0].arrival.at;
+      document.getElementById("flight-airline").textContent = vueloBarato.itineraries[0].segments[0].carrierCode;
+      document.getElementById("flight-price").textContent = vueloBarato.price.total;
+      document.getElementById("flight-currency").textContent = vueloBarato.price.currency;
 
+      document.getElementById("loading-message").style.display = "none";
+  
+      
+      document.getElementById("flight-result").classList.remove("hidden");
+      
     } catch (error) {
-      // Visualizar el error en la consola
-      console.error("Error:");
+      
+      console.error("Error:", error);
     }
   }
+  
 
   // Uso de la función para obtener un token
-  const clientId = "vFAgjkG4GNw4o8as9aSdlqhYHWvbFt9s"; // Reemplaza con tu cliente ID de Amadeus
-  const clientSecret = "GNNN7muQQqXqLjRz"; // Reemplaza con tu cliente secret de Amadeus
+  const clientId = "vFAgjkG4GNw4o8as9aSdlqhYHWvbFt9s"; 
+  const clientSecret = "GNNN7muQQqXqLjRz"; 
 
   let token = await getAmadeusToken(clientId, clientSecret);
   if (token !== null) console.log(token);
 
-  // Variable para pasajeros y obtención de fecha de mañana para el buscador de billetes
+ // Busqueda de fecha y variable pasajero
   const passengers = 1;
 
   function tomorrow() {
@@ -96,33 +118,63 @@ searchForm.addEventListener("submit", async function (e) {
 
   console.log(departureDate);
 
-  searchFlights(token, origin, destination, departureDate);
+  searchFlights(token, origin, destination, departureDate, passengers);
 });
 
+
+// Verificar que lo introducido en el input sean tres caracteres en mayusculas
+
+// document.addEventListener("DOMContentLoaded", function () {
+//   const form = document.getElementById("flight-search-form");
+//   const errorMessage = document.getElementById("error-message");
+
+//   form.addEventListener("submit", function (event) {
+//     const originInput = document.getElementById("origin");
+//     const destinationInput = document.getElementById("destination");
+
+//     const originValue = originInput.value.trim(); 
+//     const destinationValue = destinationInput.value.trim();
+
+    
+//     const uppercaseRegex = /^[A-Z]{3}$/;
+
+//     if (!uppercaseRegex.test(originValue) || !uppercaseRegex.test(destinationValue)) {
+//       event.preventDefault();
+
+     
+//       errorMessage.textContent = "¡¡¡Los campos de origen y destino deben tener exactamente 3 letras mayúsculas!!!";
+//     } else {
+      
+//       errorMessage.textContent = "";
+//     }
+//   });
+// });
 
 document.addEventListener("DOMContentLoaded", function () {
   const form = document.getElementById("flight-search-form");
   const errorMessage = document.getElementById("error-message");
+  const resultDiv = document.getElementById("flight-result");
 
   form.addEventListener("submit", function (event) {
-    const originInput = document.getElementById("origin");
-    const destinationInput = document.getElementById("destination");
-
-    const originValue = originInput.value.trim(); 
-    const destinationValue = destinationInput.value.trim();
-
     
-    const uppercaseRegex = /^[A-Z]{3}$/;
+    resultDiv.classList.add("hidden");
+    errorMessage.classList.add("hidden");
+  });
 
-    if (!uppercaseRegex.test(originValue) || !uppercaseRegex.test(destinationValue)) {
-      event.preventDefault();
 
-     
-      errorMessage.textContent = "¡¡¡Los campos de origen y destino deben tener exactamente 3 letras mayúsculas!!!";
-    } else {
-      
-      errorMessage.textContent = "";
-    }
+  const resetButton = document.querySelector(".reset");
+  resetButton.addEventListener("click", function () {
+    
+    resultDiv.classList.add("hidden");
+    errorMessage.classList.add("hidden");
+    
+    document.getElementById("flight-origin").textContent = "";
+    document.getElementById("flight-destination").textContent = "";
+    document.getElementById("flight-departure").textContent = "";
+    document.getElementById("flight-arrival").textContent = "";
+    document.getElementById("flight-airline").textContent = "";
+    document.getElementById("flight-price").textContent = "";
+    document.getElementById("flight-currency").textContent = "";
   });
 });
 
